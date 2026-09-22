c4c7398dca62fb93df88b95120796a8c4565b13f
Neyrony <sserebriy@gmail.com>
2026-09-22 12:27:02 +0300

    feat: add admin page for models

--- social_media/admin.py
+++ social_media/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from social_media.models import Profile, Post, Comment, Hashtag
+
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Hashtag)
+
+admin.site.unregister(Group)
