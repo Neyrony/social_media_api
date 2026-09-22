f1928d095d8a9805030ea9645461bbdb1a145c2f
Neyrony <sserebriy@gmail.com>
2026-09-22 12:51:23 +0300

    admin: change bare admin page to more descriptive one

--- social_media/admin.py
+++ social_media/admin.py
@@ -3,9 +3,39 @@ from django.contrib.auth.models import Group
 
 from social_media.models import Profile, Post, Comment, Hashtag
 
-admin.site.register(Profile)
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Hashtag)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("username", "bio", "user")
+    search_fields = ("username", "user__email")
+    ordering = ("username",)
+    list_per_page = 25
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "content", "owner", "created_at")
+    search_fields = ("title", "content", "owner__username")
+    list_filter = ("created_at",)
+    ordering = ("-created_at",)
+    list_per_page = 25
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("content", "owner", "post", "created_at")
+    search_fields = ("content", "owner__username")
+    list_filter = ("created_at",)
+    ordering = ("-created_at",)
+    list_per_page = 25
+
+
+@admin.register(Hashtag)
+class HashtagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    ordering = ("name",)
+    list_per_page = 25
+
 
 admin.site.unregister(Group)
