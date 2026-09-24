cc187173ea076b5e92daf4456ad7ccabd92ac049
Neyrony <sserebriy@gmail.com>
2026-09-24 13:52:04 +0300

    fix: change ordering in validate_publish_at

--- social_media/validators.py
+++ social_media/validators.py
@@ -2,12 +2,12 @@ from django.utils import timezone
 
 
 def validate_publish_at(value, exception, instance):
-    if value is not None and value < timezone.now():
-        raise exception("Publish date should be greater than the current time")
-
     if instance and (
         instance.publish_at is None or instance.publish_at < timezone.now()
     ):
         raise exception("The post is already published")
+
+    if value is not None and value < timezone.now():
+        raise exception("Publish date should be greater than the current time")
 
     return value
