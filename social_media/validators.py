c79ee7845a4ad4c28cc6e41da971d2d5d2df6de0
Neyrony <sserebriy@gmail.com>
2026-09-24 13:48:03 +0300

    fix: correct validation when post was published immediately and change time zone

--- social_media/validators.py
+++ social_media/validators.py
@@ -5,7 +5,9 @@ def validate_publish_at(value, exception, instance):
     if value is not None and value < timezone.now():
         raise exception("Publish date should be greater than the current time")
 
-    if instance and instance.publish_at and instance.publish_at < timezone.now():
+    if instance and (
+        instance.publish_at is None or instance.publish_at < timezone.now()
+    ):
         raise exception("The post is already published")
 
     return value
